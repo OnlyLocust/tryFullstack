87550d55bf0b57a24e48def85532cedf872626d7
Harsh Kr <harshchudasama4739s@gmail.com>
2024-10-04 22:37:18 +0530

    github data added

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res) => {
-    res.send('welcome')  ;
+    res.sendFile('hello boisssssss')
 })
 
 app.get('/try',(req,res) => {
-    res.send("hahahahhahhhhhhhhhhhhhhh")
+    res.send("<h1>fdhgdhghfghfghfgh</h1>")
 })
 
 app.listen(process.env.PORT , () => {

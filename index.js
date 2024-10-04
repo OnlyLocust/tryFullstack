6fe82c48bde8cbbd1becd3d0ca71e96958a64914
Harsh Kr <harshchudasama4739s@gmail.com>
2024-10-04 22:38:03 +0530

    second

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res) => {
-    res.sendFile('hello boisssssss')
+    res.send('hello boisssssss')
 })
 
 app.get('/try',(req,res) => {
